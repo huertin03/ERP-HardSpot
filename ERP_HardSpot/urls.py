3c691daf409a66c3e4b36c7fbdfed4277e27e496
Álvaro Huertas <huertin03@gmail.com>
2024-01-17 17:40:35 +0100

    Mysql Connected to Django and added empelados users

--- ERP_HardSpot/urls.py
+++ ERP_HardSpot/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from login.views import login
 
 urlpatterns = [
+    path('login/', login),
     path('admin/', admin.site.urls),
 ]
