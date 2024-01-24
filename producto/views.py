249b1bce1718f4c9f1079238dcfc4018200a54ca
Álvaro Huertas <huertin03@gmail.com>
2024-01-24 18:30:11 +0100

    Push para mi queridisimo Alejandro

--- producto/views.py
+++ producto/views.py
@@ -5,6 +5,14 @@ from producto.models import Productos
 
 def lista_productos_view(request):
     if request.user.is_authenticated:
+        if request.method == 'POST':
+            nombre = request.POST.get('nombre')
+            precio = request.POST.get('precio')
+            categoria = request.POST.get('categoria')
+            descripcion = request.POST.get('descripcion')
+            producto = Productos(nombre=nombre, descripcion=descripcion, precio=precio, categoria=categoria)
+            producto.save()
+
         user = request.user
         productos = Productos.objects.all()
         return render(request, 'producto/listaProducto.html', {'user': user, 'productos': productos})
--- proveedores/views.py
+++ proveedores/views.py
@@ -5,6 +5,14 @@ from proveedores.models import Proveedores
 
 def lista_proveedores_view(request):
     if request.user.is_authenticated:
+        if request.method == 'POST':
+            nombre = request.POST.get('nombre')
+            email = request.POST.get('email')
+            telefono = request.POST.get('telefono')
+            direccion = request.POST.get('direccion')
+            proveedor = Proveedores(nombre=nombre, email=email, telefono=telefono, direccion=direccion)
+            proveedor.save()
+
         user = request.user
         proveedores = Proveedores.objects.all()
         return render(request, 'proveedores/listaProveedores.html', {'user': user, 'proveedores': proveedores})
